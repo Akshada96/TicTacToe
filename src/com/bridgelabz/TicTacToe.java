a1709deec1e1df49a805e000a01dbf0374f2c9f3
Akshada <akshumore96@gmail.com>
2022-03-15 19:58:11 +0530

    Added UC-1

--- src/com/bridgelabz/TicTacToe.java
+++ src/com/bridgelabz/TicTacToe.java
@@ -3,5 +3,9 @@ package com.bridgelabz;
 public class TicTacToe {
     public static void main(String[] args) {
         System.out.println("Tic Tac Toe workshop");
+        char[] board = new char[10];
+        for (int i=1; i< board.length; i++) {
+            board[i] = ' ';
+        }
     }
 }
