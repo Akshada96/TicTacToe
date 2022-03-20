917239bc93df90fc0d200b56f9165ce8809b5b0f
Akshada <akshumore96@gmail.com>
2022-03-20 13:52:12 +0530

    Added UC-7 checkResult function

--- src/com/bridgelabz/TicTacToe.java
+++ src/com/bridgelabz/TicTacToe.java
@@ -4,20 +4,21 @@ import java.util.Scanner;
 
 public class TicTacToe {
     static char[] board = new char[10];
-    static char player1, computer;
+    static char player1, computer, turn;
+    static int isEnd = 0;
     public static void main(String[] args) {
         System.out.println("Tic Tac Toe workshop");
         for (int i = 1; i < board.length; i++) {
             board[i] = ' ';
         }
-        choise();
-        showBoard();
+        choice();
         toss();
-        int move = userMove();
-        isSpaceFree(move);
+        showBoard();
+        userMove();
+        checkResult();
     }
 
-    public static void choise() {
+    public static void choice() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Select either X or O ");
         player1 = sc.next().charAt(0);
@@ -39,11 +40,11 @@ public class TicTacToe {
         }
     }
 
-    public static int userMove() {
-        System.out.println("Select the index from 1 to 9 to make th move : ");
+    public static void userMove() {
+        System.out.println("Select the index from 1 to 9 to make th move for "+turn);
         Scanner sc = new Scanner(System.in);
         int move = sc.nextInt();
-        return move;
+        isSpaceFree(move);
     }
 
     public static void isSpaceFree(int move){
@@ -52,16 +53,73 @@ public class TicTacToe {
         }
         else {
             System.out.println("Selected space is already occupied, select some other index");
-            move = userMove();
-            isSpaceFree(move);
+            userMove();
         }
     }
 
     public static void toss(){
         int coin = (int) (Math.random()*2);
-        if (coin == 1)
+        if (coin == 1) {
             System.out.println("Player1 will start first");
-        else
+            turn = player1;
+        }
+            else {
             System.out.println("Computer will start first");
+            turn = computer;
+        }
+    }
+
+    public static void checkResult(){
+        // first check if any cell is blank
+        for (int i = 1; i < board.length; i++) {
+            if (board[i] == ' '){
+                //System.out.println("Change the turn");
+                isEnd = 0;
+                //Changing the turn
+                if (turn == player1)
+                    turn = computer;
+                else
+                    turn = player1;
+                break;
+            }
+        }
+        //check who won or is tie
+        int isTie = 1;
+        if (isEnd == 1) {
+            //horizontal winning positions
+            for (int i = 1; i < board.length; i = i + 3) {
+                if (board[i] == board[i + 1] && board[i + 1] == board[i + 2]) {
+                    if (board[i] == player1)
+                        System.out.println("Player1 won");
+                    else
+                        System.out.println("Computer won");
+                    isTie = 0;
+                    break;
+                }
+            }
+            //vertical winning positions
+            for (int i = 1; i < 4; i++) {
+                if (board[i] == board[i + 3] && board[i + 3] == board[i + 6]) {
+                    if (board[i] == player1)
+                        System.out.println("Player1 won");
+                    else
+                        System.out.println("Computer won");
+                    isTie = 0;
+                    break;
+                }
+            }
+            //diagonal winning position
+            if (board[1] == board[5] && board[5] == board[9] ||
+                    board[3] == board[5] && board[5] == board[7]) {
+                if (board[1] == player1 || board[3] == player1)
+                    System.out.println("Player1 won");
+                else
+                    System.out.println("Computer won");
+                isTie = 0;
+            }
+            if (isTie==1) {
+                System.out.println("Game Tie");
+            }
+        }
     }
 }
